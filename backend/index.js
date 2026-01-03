f5f33ca0e59715bdac9f2e571b5834a54c5684c2
dvlpr-ruchi <internruchi@gmail.com>
2026-01-03 11:49:47 -0800

    intial commit

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js"
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connect DB
+connectDB();
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+<h1 className="text-3xl font-bold text-center text-green-500">Contact Management System</h1>
+    </>
+  )
+}
+
+export default App
